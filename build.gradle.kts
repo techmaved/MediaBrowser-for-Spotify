5fac819866b2e9d6974f0463068a2b1335cb3142
Armin Burger <56692081+techmaved@users.noreply.github.com>
2024-08-25 13:13:58 +0200

    chore: migrate to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
-    id("io.github.reactivecircus.app-versioning") version "1.3.1" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.1.4" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.app.versioning) apply false
+    alias(libs.plugins.aboutlibraries.plugin) apply false
+    alias(libs.plugins.serialization.plugin)
 }
